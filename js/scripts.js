84e5e58f21009f9228100a0cd09d8eb748d5c020
Caitlin Hines and Shruti Priya <shruti@>
2017-01-18 11:32:41 -0800

    add first html, jquery, and start pig latin spec branching

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+var vowels = ["a", "e", "i", "o", "u"];
+var test ="hi";
+alert ("hi")
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event){
+    var wordInput = ("#input").val();
+    var wordArray = wordInput.split("");
+    var pigLatin = "ay";
+
+    for (index = 0; index <= wordArray.length; index += 1){
+      if(wordArray[index]=== "a"||wordArray[index]=== "e"||wordArray[index]=== "i"||wordArray[index]=== "o"||wordArray[index]=== "u"||wordArray[index]=== "A"||wordArray[index]=== "E"||wordArray[index]=== "I"||wordArray[index]=== "O"||wordArray[index]=== "U"
+
+      $(wordArray).push(piglatin);
+      // $(wordArray).toString();
+
+  )}
+
+
+
+   $("#output").text(output);
+});
+});
